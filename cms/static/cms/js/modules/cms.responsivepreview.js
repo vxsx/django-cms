558fa325875a794f5900e3c0b9e818fc111964d9
Vadim Sikora <vadim.sikora@divio.ch>
2017-11-15 09:13:13 +0800

    wip

---
+++ cms/static/cms/js/modules/cms.responsivepreview.js
@@ -0,0 +1,155 @@
+import $ from 'jquery';
+import srcDoc from 'srcdoc-polyfill';
+import { $window, $document } from './cms.base';
+import Resizer from './cms.resizer';
+
+// TODO prevent structure board from opening
+
+let markupPromise;
+const getCurrentMarkup = () => {
+    var newDoc = new DOMParser().parseFromString(document.documentElement.outerHTML, 'text/html');
+
+    $(newDoc)
+        .find('html')
+        .css('margin-top', '0')
+        .removeClass(
+            'cms-overflow cms-responsive-overflow ' +
+            'cms-structure-mode-content cms-structure-mode-structure cms-toolbar-expanded cms-ready'
+        )
+        .find('#cms-top, [data-cms]').remove();
+    return Promise.resolve(newDoc.documentElement.outerHTML);
+    // TODO reset back to ?edit afterwards
+    // return $.ajax({
+    //     url: window.location.pathname + '?toolbar_off'
+    // }).then(markup => markup);
+};
+const getOrAddFrame = () => {
+    let frame = $('.cms-responsive-preview-frame');
+
+    if (frame.length) {
+        return frame[0];
+    }
+
+    frame = $('<iframe class="cms-responsive-preview-frame"></iframe>');
+
+    return frame[0];
+};
+const preventScrolling = () => $('html').addClass('cms-responsive-overflow');
+const allowScrolling = () => $('html').removeClass('cms-responsive-overflow');
+
+class ResponsivePreview {
+    constructor() {
+        this.isOpen = false;
+
+        this._setupUI();
+        this._events();
+    }
+
+    _setupUI() {
+        this.ui = {
+            trigger: $('.cms-responsive-view')
+        };
+    }
+
+    _events() {
+        this.ui.trigger.on(ResponsivePreview.click, () => this.toggleResponsiveView());
+
+        $window.on('cms-content-refresh', () => {
+            markupPromise = null;
+            this._loadMarkup().then(markup => {
+                const frame = getOrAddFrame();
+
+                srcDoc.set(frame, markup);
+            });
+        });
+    }
+
+    toggleResponsiveView() {
+        if (this.isOpen) {
+            this.isOpen = false;
+            this.ui.trigger.removeClass('cms-btn-active');
+            return this._destroyUI();
+        }
+
+        this.isOpen = true;
+        this.ui.trigger.addClass('cms-btn-active');
+        return this._loadMarkup()
+            .then(this._buildUI.bind(this));
+    }
+
+    _loadMarkup() {
+        if (!markupPromise) {
+            CMS.API.Toolbar.showLoader();
+            markupPromise = getCurrentMarkup().then(r => {
+                CMS.API.Toolbar.hideLoader();
+                return r;
+            });
+        }
+        return markupPromise;
+    }
+
+    _buildUI(markup) {
+        const frame = getOrAddFrame();
+
+        preventScrolling();
+        srcDoc.set(frame, markup);
+
+        this.resizer = new Resizer({
+            frame: frame
+        });
+
+        $(frame).on('load', () => {
+            setTimeout(() => {
+                $(frame.contentDocument.documentElement)
+                    .on('pointerover pointerout touchstart', '.cms-plugin', e => {
+                        e.stopPropagation();
+                        var event = new $.Event(e.type);
+
+                        if (!e.currentTarget.className) {
+                            return;
+                        }
+
+                        let xAdjustment = this.resizer.ui.wrapper.offset().left;
+                        let yAdjustment = this.resizer.ui.wrapper.offset().top;
+                        // FIXME nested plugins are not highlighted
+
+                        event.target = $('.' + e.currentTarget.className.split(/\s+/g).join('.'))[0];
+                        event.originalEvent = {
+                            pageX: e.clientX + xAdjustment,
+                            pageY: e.clientY + yAdjustment
+                        };
+
+                        $document.trigger(event);
+                    })
+                    .find('body')
+                    .on('mousemove', e => {
+                        var event = new $.Event(e.type);
+
+                        let xAdjustment = this.resizer.ui.wrapper.offset().left;
+                        let yAdjustment = this.resizer.ui.wrapper.offset().top;
+
+                        event.target = e.target;
+                        event.pageX = e.clientX + xAdjustment;
+                        event.pageY = e.clientY + yAdjustment;
+
+                        $document.find('body').trigger(event);
+                    })
+                    .find('.cms-plugin').on('click dblclick', e => {
+                        $('.' + e.target.className.split(/\s+/g).join('.')).trigger(e.type);
+                    });
+            }, 50); // eslint-disable-line no-magic-numbers
+        });
+    }
+
+    _destroyUI() {
+        this.resizer.destroy();
+        this.resizer = null;
+        allowScrolling();
+
+        return Promise.resolve();
+    }
+}
+
+ResponsivePreview.click = 'click.cms.responsive';
+
+export default ResponsivePreview;
